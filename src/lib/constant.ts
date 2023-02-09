0373aeb604f1ab49e3c3a2238cbc62ca15c5f1ec
Mohamad Samardeh Haghighi Fard <pmbeman@pm.me>
2023-02-10 00:41:55 +0330

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -1,6 +1,6 @@
 export const CONSUMET_URL = "https://consumet.irn.workers.dev";
 export const ENIME_URL = "https://enimemoe.irn.workers.dev/";
-export const CORS_PROXY = "https://cors.consumet.stream/";
+export const CORS_PROXY = "https://consumetstream.irn.workers.dev/";
 export const PAGE = 1;
 export const GOGO_PROVIDER = "gogoanime";
 export const ZORO_PROVIDER = "zoro";
